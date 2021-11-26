4fc7d03c447af9507b5846c428c43ae7c7d64794
Vitaliy Yurchenko <world-rom@yandex.ru>
2021-11-26 12:30:01 +0400

    Добавление изменений в script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+const title = 1; 
+const screens = 2; 
+const screenPrice = 3; 
+const rollback = 4; 
+const ullPrice = 5; 
+const adaptive = 6;
+
 alert('Уже сложно');
+console.log('Ну правда сложно');
