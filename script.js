af0f1842f5c660cbba14b1034f55203fcd3506bb
Vitaliy Yurchenko <world-rom@yandex.ru>
2021-11-29 23:58:44 +0400

    Домашнее задание 3 урока: beta

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-let title = "Homework Lesson 2";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 235;
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = prompt('Сколько будет стоить данная работа?');
 let rollback = 15; //любое число от 1 до 100
-let fullPrice = 60000;//любое число (сколько хотите заработать)
-let adaptive = false; //булевое значение
-
-console.log(typeof title);
+let adaptive = confirm("Нужен ли адаптив на сайте?"); //булевое значение
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = prompt("Сколько это будет стоить?");
+let service2 = prompt("Какой ещё дополнительный тип услуги Вам нужен?");
+let servicePrice2 = prompt("Сколько так же это будет стоить?");
+let fullPrice = parseInt(screenPrice) + parseInt(servicePrice1) + parseInt(servicePrice2);//любое число (сколько хотите заработать)
+let servicePercentPrice = Math.ceil(fullPrice - (fullPrice * 0.30)); //30% посреднику
+/*console.log(typeof title);
 console.log(typeof fullPrice);
 console.log(typeof adaptive);
 
@@ -15,4 +19,41 @@ console.log("Стоимость верстки экранов " + screenPrice + 
 console.log("Стоимость разработки сайта " + fullPrice + " рублей");
 
 console.log(screens.toLowerCase().split(", "));
-console.log("Процент отката посреднику за работ: " + fullPrice * (rollback/100));
+console.log("Процент отката посреднику за работ: " + fullPrice * (rollback/100));*/
+console.log('Название проекта: ' + title);
+console.log(screens.split(', '));
+console.log('Стоимость работы: ' + parseInt(screenPrice));
+switch (adaptive) {
+    case true:
+        console.log('Адаптив должен быть!');
+        break
+    default: 
+        console.log('Адаптива не будет');
+};
+
+//если будет пустая строка
+servicePrice1 === '' ? servicePrice1 = 0 : servicePrice1;
+servicePrice2 === '' ? servicePrice2 = 0 : servicePrice2;
+//________________________
+console.log('Дополнительный тип услуги: ' + service1 + ". Данная услуга будет стоить " + parseInt(servicePrice1));
+console.log('Ещё один дополнительный тип услуги: ' + service2 + ". Данная услуга будет стоить " + parseInt(servicePrice2));
+
+
+console.log("fullPrice: " + fullPrice);
+console.log('Итоговая стоимость за вычетом отката посреднику: ' + servicePercentPrice);
+
+switch(true) {
+    case 30000 <= fullPrice:
+        console.log("Даем скидку в 10%");
+        break
+    case 15000 <= fullPrice && fullPrice < 30000:
+        console.log("Даем скидку в 5%");
+        break
+    case 0 <= fullPrice && fullPrice < 15000: //меньше 15000 и больше 0
+        console.log("Скидка не предусмотрена");
+        break
+    default: 
+        console.log('Что то пошло не так'); 
+}
+console.log(typeof servicePrice1);
+console.log(typeof screenPrice);
